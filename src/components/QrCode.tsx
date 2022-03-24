2b30e4ec0f80f8f9c20e05d2006e5e59f2dd4f75
Israel Ferreira <israel.fsantos27@gmail.com>
2022-03-24 17:05:31 -0300

    edit

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,8 +1,9 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import { qrCode } from "../images";
 
 const QrCode: React.FC = () => {
-  return <Grid width="100%" display="flex" justifyContent="center">Vai ter o QrCode aqui</Grid>;
+  return <Grid width="100%" display="flex" justifyContent="center"><img width={520} src={qrCode} alt="qrcode" /></Grid>;
 };
 
 export default QrCode;
---
+++ src/images/index.tsx
@@ -0,0 +1,1 @@
+export { default as qrCode } from "./qrcode.jpg";
